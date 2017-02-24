c22e0160d009a5ab4ddb41b4c620547694f8f12c
sashidharan <sashi.it4@gmail.com>
2017-02-24 15:32:31 +0530

    changesn i

--- www/js/services.js
+++ www/js/services.js
@@ -9,18 +9,21 @@ angular.module('starter.services', [])
     name: 'Bristol',
     url: 'scan',
     style: 'bristol',
+    siteId: 568577,
     img: 'img/bristol.png'
   }, {
     id: 1,
     name: 'Notting Ham',
     url: 'info',
     style: 'hulldaily',
+    siteId: 568577,
     img: 'img/nottingham.png'
   }, {
     id: 2,
     name: 'Hull Daily Mail',
     url: 'info',
     style: 'nottingham',
+    siteId: 568577,
     img: 'img/hulldaily.png'
   }];
 
@@ -40,4 +43,40 @@ angular.module('starter.services', [])
       return null;
     }
   };
-});
+})
+.factory('AjaxMethod',  ['$http', function($http) {
+    var urlBase = 'https://api.localworld.co.uk/articles/%SITE_ID%/detail/%ARTICLE_ID%';
+    return {
+        callmethod:function(){
+            alert('haide');
+        },
+        getMethod:function (parameter) {
+            urlBase = 
+            try {
+                $http.get(urlBase)
+                .success(function (response) {
+                    return response;
+                })
+                .error(function(err) {
+                    console.log(err);
+                });
+            } catch(e) {
+                console.log(e);
+            }
+        },
+        postMethod:function (parameter) {
+            try {
+                $http.post(urlBase, parameter)
+                .success(function (response) {
+                    return response;
+                })
+                .error(function(err) {
+                    console.log(err);
+                });
+            } catch(e) {
+                console.log(e);
+            }
+        }
+    };
+}]);
+;
